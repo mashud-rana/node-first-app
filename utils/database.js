4acd4e329df1d9d980b50c8bc99be4a059c6a0da
mashud-rana <mashud624496@gmail.com>
2023-05-06 01:32:14 +0600

    ..

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,23 @@
-// get the client
-const mysql = require('mysql2');
+// // get the client
+// const mysql = require('mysql2');
 
-// Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'admin',
-  database: 'node_schema',
-  password:"password",
+// // Create the connection pool. The pool-specific settings are the defaults
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'admin',
+//   database: 'node_schema',
+//   password:"password",
+// });
+
+// module.exports=pool.promise();
+
+
+const Sequelize=require('sequelize');
+
+
+const sequelize=new Sequelize('node_schema','admin','password',{
+  host:'localhost',
+  dialect:'mysql'
 });
 
-module.exports=pool.promise();
+module.exports=sequelize;
